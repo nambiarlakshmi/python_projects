4504f112b3a572cf66c8ab4a7f4fe12bf6880825
nambiarlakshmi <nambiarlakshmi@outlook.com>
2026-05-02 16:44:51 -0500

    Add files via upload

--- bit_play_1.py
+++ bit_play_1.py
@@ -25,3 +25,23 @@ num2 = 4
 print("\nnum1 << 1 =", num1 << 1)
 # Using Left Shift operator on num2
 print("\nnum2 << 1 =", num2 << 1)
+
+def isEvenOdd(n) :
+    if (n ^ 1 == n + 1) :
+        return True;
+    else :
+        return False;
+number = int(input("Enter your number : "))
+if isEvenOdd(number):
+    print(number," is Even")
+else:
+    print(number," is Odd")
+
+def numberOfBits(n):
+    count = 0
+    while (n):
+        count += 1
+        n >>= 1         
+    return count 
+number = int(input("Enter your number : "))
+print("Total bits : ",numberOfBits(number))
